8e1d223154b9eaf97abcf9c170eeb54765185384
Crystal-girl <letsvfly@gmail.com>
2019-12-24 19:58:25 +0800

    leetcode-206

--- linked_list/reverseList.py
+++ linked_list/reverseList.py
@@ -3,7 +3,7 @@ leetcode 206
 reverse List
 """
 
-def reverseList(self, head):
+def reverse_list(self, head):
     cur, prev = head, None
     while cur:
         cur.next, prev, cur = prev, cur, cur.next
