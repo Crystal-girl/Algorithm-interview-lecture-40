3263cf0f19dc530308dbe238109efe9bf44044f9
Crystal-girl <letsvfly@gmail.com>
2019-12-24 10:29:19 +0800

    leetcode-20

---
+++ validParentheses.py
@@ -0,0 +1,14 @@
+"""
+leetcode 20
+Valid Parentheses
+"""
+
+def isValid(self, s):
+    stack = []
+    paren_map = {')': '(', ']': '[', '}': '{'}
+    for c in s:
+        if c not in paren_map:
+            stack.append(c)
+        elif not stack or paren_map[c] != stack.pop():
+            return False
+    return not stack
