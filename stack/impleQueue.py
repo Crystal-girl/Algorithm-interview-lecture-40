13756a4f602b0fe5b7cf4514b2ebd4a7fcbe3d28
Crystal-girl <Crystal-girl@users.noreply.github.com>
2019-12-29 23:51:36 +0800

    leetcode-232

--- stack/impleQueue.py
+++ stack/impleQueue.py
@@ -7,22 +7,22 @@ def __init__(self):
     self.instack = []
     self.outstack = []
 
-def push(self, x):
+def push(self, x: int) -> None:
     self.instack.append(x)
 
-def pop(self):
+def pop(self) -> int:
     if not self.outstack:
         while self.instack:
             self.outstack.append(self.instack.pop())
     return self.outstack.pop()
 
-def peek(self):
+def peek(self) -> int:
     if not self.outstack:
         while self.instack:
             self.outstack.append(self.instack.pop())
     return self.outstack[-1]
 
-def empty(self):
+def empty(self) -> bool:
     if not self.instack and not self.outstack:
         return True
     return False
