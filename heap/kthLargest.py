3d65e7654792dad7b76ce60d20ec4205f3eb354f
Crystal-girl <Crystal-girl@users.noreply.github.com>
2019-12-29 23:12:19 +0800

    leetcode-703

--- heap/kthLargest.py
+++ heap/kthLargest.py
@@ -4,15 +4,16 @@ Kth Largest Element in a Stream
 """
 
 import heapq
+from typing import List
 
-def __init__(self, k, nums):
+def __init__(self, k: int, nums: List[int]):
     self.heap = nums
     self.k = k
     heapq.heapify(self.heap)
     while len(self.heap) > k:
         heapq.heappop(self.heap)
 
-def add(self, val):
+def add(self, val: int) -> int:
     if len(self.heap) < self.k:
         heapq.heappush(self.heap, val)
     elif val > self.heap[0]:
