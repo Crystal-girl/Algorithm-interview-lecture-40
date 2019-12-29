e4d743bb35008077e901a315c438602a6f4d2b9f
Crystal-girl <Crystal-girl@users.noreply.github.com>
2019-12-29 23:21:50 +0800

    leetcode-225

--- queue/impleStack.py
+++ queue/impleStack.py
@@ -7,16 +7,16 @@ import collections
 def __init__(self):
     self.stack = collections.deque
 
-def push(self, x):
+def push(self, x: int) -> None:
     self.stack.append(x)
     for _ in range(len(self.stack) - 1):
         self.stack.append(self.stack.popleft())
 
-def pop(self):
+def pop(self) -> int:
     return self.stack.popleft()
 
-def top(self):
+def top(self) -> int:
     return self.stack[0]
 
-def empty(self):
+def empty(self) -> bool:
     return not self.stack
