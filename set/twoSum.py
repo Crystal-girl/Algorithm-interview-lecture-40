590f2e781d28a8c86e4fa98db419acaaceac7292
Crystal-girl <letsvfly@gmail.com>
2019-12-30 16:51:51 +0800

    leetcode-1

--- set/twoSum.py
+++ set/twoSum.py
@@ -2,5 +2,13 @@
 leetcode 1
 Two Sum
 """
+from typing import List
 
-def twoSum()
+
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+    set1 = set()
+    for i, item in enumerate(nums):
+        if target - item in set1:
+            return [nums.index(target - item), i]
+        set1.add(item)
+    return []
